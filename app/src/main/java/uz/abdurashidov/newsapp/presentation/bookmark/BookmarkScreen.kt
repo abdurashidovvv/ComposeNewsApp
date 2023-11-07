5d45c28942d8a41a7277be854c439b3735d07906
Saydullo Abdurashidov <abdurashidovsaydullo35@gmail.com>
2023-11-07 15:22:39 +0300

    14.2 Bookmark Screen

---
+++ app/src/main/java/uz/abdurashidov/newsapp/presentation/bookmark/BookmarkScreen.kt
@@ -0,0 +1,43 @@
+package uz.abdurashidov.newsapp.presentation.bookmark
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
+import uz.abdurashidov.newsapp.R
+import uz.abdurashidov.newsapp.presentation.Dimens.MediumPadding1
+import uz.abdurashidov.newsapp.presentation.common.ArticleList
+import uz.abdurashidov.newsapp.presentation.nvgraph.Route
+
+@Composable
+fun BookMarkScreen(
+    state: BookmarkState,
+    navigate: (String) -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .statusBarsPadding()
+            .padding(top = MediumPadding1, end = MediumPadding1, start = MediumPadding1)
+    ) {
+        Text(
+            text = "Bookmark",
+            style = MaterialTheme.typography.displayMedium.copy(fontWeight = FontWeight.Bold),
+            color = colorResource(
+                id = R.color.text_title
+            )
+        )
+
+        Spacer(modifier = Modifier.height(MediumPadding1))
+
+        ArticleList(articles = state.articles, onClick = { navigate(Route.DetailsScreen.route) })
+    }
+}
--- app/src/main/java/uz/abdurashidov/newsapp/presentation/common/ArticleList.kt
+++ app/src/main/java/uz/abdurashidov/newsapp/presentation/common/ArticleList.kt
@@ -17,6 +17,30 @@ import uz.abdurashidov.newsapp.presentation.Dimens.MediumPadding1
 @Composable
 fun ArticleList(
     modifier: Modifier = Modifier,
+    articles: List<Article>,
+    onClick: (Article) -> Unit
+) {
+
+    LazyColumn(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(MediumPadding1),
+        contentPadding = PaddingValues(all = ExtraSmallPadding2)
+    ) {
+        items(count = articles.size) {
+
+            val article = articles[it]
+            ArticleCard(article = article, onClick = { onClick(article) })
+
+
+        }
+    }
+
+
+}
+
+@Composable
+fun ArticleList(
+    modifier: Modifier = Modifier,
     articles: LazyPagingItems<Article>,
     onClick: () -> Unit
 ) {
@@ -24,7 +48,7 @@ fun ArticleList(
 
     if (handlePagingResult) {
         LazyColumn(
-            modifier = Modifier.fillMaxSize(),
+            modifier = modifier.fillMaxSize(),
             verticalArrangement = Arrangement.spacedBy(MediumPadding1),
             contentPadding = PaddingValues(all = ExtraSmallPadding2)
         ) {
