63610cb82ba2a647e71460c38d63e85dcd29dc5d
Saydullo Abdurashidov <abdurashidovsaydullo35@gmail.com>
2023-11-07 11:19:31 +0300

    12.0 DetailScreen Component Top Bar

---
+++ app/src/main/java/uz/abdurashidov/newsapp/presentation/detail/components/DetailsTopBar.kt
@@ -0,0 +1,77 @@
+package uz.abdurashidov.newsapp.presentation.detail.components
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import uz.abdurashidov.newsapp.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailsTopBar(
+    onBrowsingClick: () -> Unit,
+    onShareClick: () -> Unit,
+    onBookmarkClick: () -> Unit,
+    onBackClick: () -> Unit
+) {
+
+    TopAppBar(
+        title = {},
+        modifier = Modifier.fillMaxWidth(),
+        colors = TopAppBarDefaults.mediumTopAppBarColors(
+            containerColor = Color.Transparent,
+            actionIconContentColor = colorResource(id = R.color.body),
+            navigationIconContentColor = colorResource(id = R.color.body)
+        ),
+        navigationIcon = {
+            IconButton(onClick = onBackClick) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_back_arrow),
+                    contentDescription = null
+                )
+            }
+        },
+        actions = {
+            IconButton(onClick = onBookmarkClick) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_bookmark),
+                    contentDescription = null
+                )
+            }
+            IconButton(onClick = onShareClick) {
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    contentDescription = null
+                )
+            }
+            IconButton(onClick = onBrowsingClick) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_network),
+                    contentDescription = null
+                )
+            }
+        }
+    )
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DetailsTopBarPreview() {
+    DetailsTopBar(
+        onBrowsingClick = { /*TODO*/ },
+        onShareClick = { /*TODO*/ },
+        onBookmarkClick = { /*TODO*/ }) {
+        
+    }
+}
